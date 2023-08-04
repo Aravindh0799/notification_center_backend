83fe6e98fb24c60324318b624ec0587d5f7e7180
Aravindh0799 <aravindhan0799@gmail.com>
2023-08-04 21:01:31 +0530

    db..

--- backend/conn.js
+++ backend/conn.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 console.log("hi")
-mongoose.connect('mongodb+srv://Aravindh:Aravindh@cluster0.f7btaep.mongodb.net/?retryWrites=true&w=majority/myapp1').then(()=>{
+mongoose.connect('mongodb+srv://Aravindh:Aravindh@cluster0.f7btaep.mongodb.net/myapp1').then(()=>{
     console.log('DB connection succesfull')
 }).catch((e)=>{
     console.log('error occured', e)
