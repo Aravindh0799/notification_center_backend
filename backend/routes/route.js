ccddfeb7ebd6521317074001d388eebbf0d87c38
Aravindh0799 <aravindhan0799@gmail.com>
2023-08-04 20:48:13 +0530

    ..

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const bcrypt  = require('bcrypt')
-const nodemailer = require('nodemailer');
-const jwt = require('jsonwebtoken')
-const otp = require('otp-generator')
-const JWT_SECRET = "ciwbuconciwevccwu1229238c/idb871cb91383hc}28vwrgbw8b748{62[]()69cwv";
+// const bcrypt  = require('bcrypt')
+// const nodemailer = require('nodemailer');
+// const jwt = require('jsonwebtoken')
+// const otp = require('otp-generator')
+// const JWT_SECRET = "ciwbuconciwevccwu1229238c/idb871cb91383hc}28vwrgbw8b748{62[]()69cwv";
 const student = require('../schema/students');
 
 router.post('/register',async(req,res)=>{
